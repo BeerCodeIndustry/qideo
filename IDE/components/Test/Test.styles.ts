1773eea05bf1c16cd4bca1ccbb614a63b191cf83
ordinaryask <ordinaryask@gmail.com>
2022-10-03 11:32:53 +0200

    feat: add cf

---
+++ IDE/components/Test/Test.styles.ts
@@ -0,0 +1,8 @@
+import { theme } from '@beercode/common-frontend'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: ${theme.colors.BLUE_600};
+  width: 300px;
+  height: 300px;
+`
