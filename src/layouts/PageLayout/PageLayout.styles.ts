d794f192ef5b51e9873cc41cb3bb6fcc88f0a236
Vlad <88458763+VladPlakhotnik@users.noreply.github.com>
2022-10-16 16:24:46 +0200

    feat: create layout (#11)

---
+++ src/layouts/PageLayout/PageLayout.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Flexbox } from '@beercode/common-frontend'
+
+export const Container = styled(Flexbox)``
+export const PageLayout = styled(Flexbox)`
+  width: 1440px;
+  padding: 0 2rem 0 2rem;
+`
