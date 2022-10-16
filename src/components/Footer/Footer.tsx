d7efc18631b4dc31e6f9e99035b8761060c142a9
Vlad <pifboostz@gmail.com>
2022-10-16 12:23:25 +0200

    fix: fix src Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,56 @@
+import {
+  ContainerFooter,
+  SocialButtons,
+  AllRightsFooter,
+  ContainerSections,
+  ContainerSection,
+  LinksSections,
+  Container,
+  FooterLogoSection,
+} from './Footer.styled'
+
+export const Footer: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <ContainerSection>
+          <FooterLogoSection>
+            <a>Logo</a>
+            <AllRightsFooter>
+              A good design is not only aesthetically pleasing, but also
+              functional. It should be able to solve the problem
+            </AllRightsFooter>
+          </FooterLogoSection>
+          <div>
+            <p>Sections</p>
+            <ContainerSections>
+              <LinksSections>
+                <a>Home</a>
+                <a>Home</a>
+                <a>Home</a>
+              </LinksSections>
+              <LinksSections>
+                <a>Home</a>
+                <a>Home</a>
+                <a>Home</a>
+              </LinksSections>
+              <LinksSections>
+                <a>Home</a>
+                <a>Home</a>
+                <a>Home</a>
+              </LinksSections>
+            </ContainerSections>
+          </div>
+        </ContainerSection>
+        <ContainerFooter>
+          <AllRightsFooter>All Rights Reservd Inkyy.com 2022</AllRightsFooter>
+          <div>
+            <SocialButtons>Dribble</SocialButtons>
+            <SocialButtons>Facebook</SocialButtons>
+            <SocialButtons>Linkedin</SocialButtons>
+          </div>
+        </ContainerFooter>
+      </Container>
+    </>
+  )
+}
