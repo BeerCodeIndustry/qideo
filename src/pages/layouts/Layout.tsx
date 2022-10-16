33172408d416ff987b3f1e66e10c1c476eaf8ca6
Vlad <pifboostz@gmail.com>
2022-10-16 11:41:05 +0200

    fix: fix src layout

--- src/pages/layouts/Layout.tsx
+++ src/pages/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import { Footer } from 'src/components/Footer/Footer'
+import { Footer } from '../../components/Footer/Footer'
 import { LayoutFunc } from './Layout.types'
 
 export const Layout: React.FC<LayoutFunc> = ({ children }) => {
