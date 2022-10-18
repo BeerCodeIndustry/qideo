05fc3859169a0aef47955c2607264b2ba4f60bc5
Anton Avsioukov <anton.avsioukov@gmail.com>
2022-10-18 19:51:39 +0300

    feat: globalTheme

--- src/components/QideoProvider/QideoProvider.component.tsx
+++ src/components/QideoProvider/QideoProvider.component.tsx
@@ -1,19 +1,18 @@
 import { Modal } from '@beercode/common-frontend'
 import { useDispatch, useSelector } from 'react-redux'
 import { closeModal } from 'reducers/common.reducer'
-import { themes } from 'src/constants/theme'
 import { StateType } from 'src/redux'
 import { ThemeProvider } from 'styled-components'
 
-import { ThemeType } from '../../types/theme'
 import { QideoProviderProps } from './QideoProvider.types'
 
 export const QideoProvider: React.FC<QideoProviderProps> = ({ children }) => {
   const modal = useSelector((state: StateType) => state.common.modal)
+  const theme = useSelector((state: StateType) => state.common.theme)
   const dispatch = useDispatch()
 
   return (
-    <ThemeProvider theme={themes[ThemeType.DARK]}>
+    <ThemeProvider theme={theme}>
       {modal && <Modal onClose={() => dispatch(closeModal())} {...modal} />}
       {children}
     </ThemeProvider>
